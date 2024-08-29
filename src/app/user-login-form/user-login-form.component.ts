162b21cd1fd0480b453c25e84ac9cb76c9fd08c4
alexmathews89 <alexmathews@live.com>
2024-08-28 22:52:52 -0700

    added fetch service registration and login forms

---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrl: './user-login-form.component.scss',
+})
+export class UserLoginFormComponent {
+  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar
+  ) {}
+
+  ngOnInit(): void {}
+
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe(
+      (response) => {
+        this.dialogRef.close();
+        console.log(response);
+        this.snackBar.open('user logged in successfully!', 'OK', {
+          duration: 2000,
+        });
+      },
+      (response) => {
+        console.log(response);
+        this.snackBar.open(response, 'OK', {
+          duration: 2000,
+        });
+      }
+    );
+  }
+}
